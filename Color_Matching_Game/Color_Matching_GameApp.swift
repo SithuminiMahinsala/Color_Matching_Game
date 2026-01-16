f705d0f948c698f755cbbc40e00d1370eb14749f
Sithumini Mahinsala <sithuminimahinsala917@gmail.com>
2026-01-16 10:29:52 +0530

    Initial Commit

---
+++ Color_Matching_Game/Color_Matching_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Matching_GameApp.swift
+//  Color_Matching_Game
+//
+//  Created by COBSCCOMP242P-001 on 2026-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Color_Matching_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Color_Matching_Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Color_Matching_Game
+//
+//  Created by COBSCCOMP242P-001 on 2026-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
